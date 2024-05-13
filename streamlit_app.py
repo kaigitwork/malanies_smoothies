71a1fc410839ce88d8a7c75ea4f9f28d3de3c485
kaigitwork <natevictorlee336@outlook.com>
2024-05-13 15:13:49 +0800

    Update streamlit_app.py
    edited fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options") \
 
 # st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas() 
-st.dataframe(pd_df)
-
-st.stop();
+# st.dataframe(pd_df)
+# st.stop();
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be: ", name_on_order)
@@ -58,6 +57,9 @@ if options:
 
     for fruit_chosen in options:
         ingredient_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition information')
         # New section to display fruityvice nutritions
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
