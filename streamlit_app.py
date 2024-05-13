aeae9145281e26f05f9d62ab3cf8cec6d0b219b5
kaigitwork <natevictorlee336@outlook.com>
2024-05-13 15:56:52 +0800

    Update streamlit_app.py
    edited write method

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if options:
            session.sql(update_stmt).collect()
            search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0] 
 
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition information')
             
