a4e230093b0d981a108a1b7f38dd05c6658c71db
kaigitwork <natevictorlee336@outlook.com>
2024-05-13 15:34:39 +0800

    Update streamlit_app.py
    remove testing lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,9 +66,7 @@ if options:
 
     for fruit_chosen in options:
         ingredient_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write(search_on)
-        st.stop()
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0] 
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         print(search_on)
