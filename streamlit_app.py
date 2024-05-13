e475042f0174982090fdd0dea0067195afebd3eb
kaigitwork <natevictorlee336@outlook.com>
2024-05-13 15:08:11 +0800

    Update streamlit_app.py
    streamlit df to pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col 
 # from snowflake.snowpark.context import get_active_session
 
@@ -19,7 +20,10 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options") \
                       .select(col("FRUIT_NAME"), col('SEARCH_ON'))
 
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas() 
+st.dataframe(pd_df)
+
 st.stop();
 
 name_on_order = st.text_input("Name on Smoothie")
